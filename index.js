624d932ca620d1662687733618bb7c4cb85cf605
Fathy Adel BK <fathyadel315@gmail.com>
2023-06-21 10:01:47 +0300

    add javascript code

---
+++ index.js
@@ -0,0 +1,18 @@
+const number = 610;
+const string = "some text";
+
+const human = {
+  first: "Andrie",
+  last: "Neagoie",
+};
+
+function subtractTwo(num) {
+  return num - 2;
+}
+
+function calculate() {
+  const sumTotal = 4 + 5;
+  return subtractTwo(sumTotal);
+}
+
+calculate();
